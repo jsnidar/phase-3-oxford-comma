dd6aa548f82646c002f520b19401cd377db37e71
Jeremiah <jsnidar@gmail.com>
2021-10-14 17:48:11 -0500

    commit tree 264663de53950ed9a4ba43e2399a6dabfd372b92

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,7 @@
+require 'pry'
 def oxford_comma(array)
-
+    if array.length == 1 then array.join
+    elsif array.length == 2 then array.join(" and ")
+    else "#{array[0...-1].join(", ")}, and #{array[-1]}"
+    end
 end
